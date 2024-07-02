5c99121aaaee1aa6ad04c6290ca49d761abca4c1
Marlonbon3 <al22304010@utslrc.edu.mx>
2024-07-02 02:57:40 -0700

    titulo pagina y actualizacion de jose

--- src/firebase/utils.jsx
+++ src/firebase/utils.jsx
@@ -28,7 +28,9 @@ export const unflattenItems = (doc, demo) => {
 
     if (bid === 0) {
       const { amount, endTime, ...itemData } = value;
-      items[item] = { ...items[item], ...itemData, startingPrice: amount, endTime: endTime.toDate() };
+      const endTimeDate = new Date(endTime);
+      items[item] = { ...items[item], ...itemData, startingPrice: amount, endTime: endTimeDate }
+
       if (demo) {
         const now = new Date();
         items[item].endTime = new Date(
