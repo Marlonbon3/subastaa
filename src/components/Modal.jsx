cdd0ec3c69901077ec25e243f05f6ba09268cc14
Diaflosno <al22304048@utslrc.edu.mx>
2024-07-03 09:59:31 -0700

    Quitando los console.log que hace que pete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -163,9 +163,6 @@ useEffect(() => {
 }, [topBidders]);
 let nowTime = new Date().getTime();
 
-console.log("usuario de ahorita",auth.currentUser.uid)
-console.log("el top bidder number 1", topBidders[0]?.uid)
-
   return (
     <Modal type={ModalTypes.ITEM} title={activeItem.title}>
       <div className="modal-body">
